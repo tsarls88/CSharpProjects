110ed6aa94e9848f515bd203c175b65819aad296
tsarls <125039671+tsarls88@users.noreply.github.com>
2025-05-07 22:11:47 +0800

    Add files via upload

---
+++ Project_2/Project_2/Project_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace DiceGame
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+
+            int playerone;
+            int playertwo;
+
+            int playerOnePts = 0; 
+            int playertwoPts = 0;
+            
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice");
+                Console.ReadKey();
+
+                playerone = random.Next(1 ,10);
+                Console.WriteLine("You rolled a Dice: " + playerone );
+
+                Console.WriteLine(".....");
+                System.Threading.Thread.Sleep(1000);
+
+                playertwo = random.Next(1 ,10);
+                Console.WriteLine("Enemy Rolled a Dice: " + playertwo);
+
+                if(playerone > playertwo)
+                {
+                    playerOnePts++;
+                    Console.WriteLine("Player one wins this round!");
+                    Console.WriteLine("......");
+                    System.Threading.Thread.Sleep(1000);
+                }else if(playerone < playertwo)
+                {
+                    Console.WriteLine("......");
+                    System.Threading.Thread.Sleep(1000);
+                    playertwoPts++;
+                    Console.WriteLine("Player two wins this Round!");
+                }
+                else
+                {
+                    Console.WriteLine("......");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("Draw");
+                }
+                Console.WriteLine("The Scores is now - PLAYER ONE: " + playerOnePts + ". PLAYER TWO: " + playertwo + ".");
+                Console.WriteLine();
+            }
+            if (playerOnePts > playertwoPts)
+            {
+                Console.WriteLine("PLAYER ONE WINS!");
+            }
+            else if (playerOnePts < playertwoPts) {
+                Console.WriteLine("PLAYER TWO WINS!");
+            }
+            else
+            {
+                Console.WriteLine("NO ONE WINS!");
+            }
+                Console.ReadKey();
+        }
+    }
+}
