d96f4d64f45cf381d277eab5c7144c264819a846
tsarls <125039671+tsarls88@users.noreply.github.com>
2025-07-05 05:32:06 -0700

    Add files via upload

---
+++ QuizGame/quiz_game/quiz_game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+
+string[] text = File.ReadAllLines("questions.txt");
+
+List<string> questions = new List<string>();  
+List<string> answers = new List<string>();
+
+for (int i = 0; i < text.Length; i++)
+{
+    if( i % 4 == 0)
+    
+        questions.Add(text[i]);
+    else
+ 
+        answers.Add(text[i]);   
+}
+int questionsIndex = 0;
+int answersIndex = 0;
+int scores = 0;
+
+while(questionsIndex < questions.Count)
+{
+    Console.WriteLine(questions[questionsIndex]);
+    questionsIndex++;
+
+    int correctAnswerIndex = 0; 
+    for (int i = 0; i < 3; i++)
+    {
+        if (answers[answersIndex].StartsWith(">"))
+            correctAnswerIndex = i + 1;
+
+        Console.WriteLine(i + 1 + "." + answers[answersIndex].Replace(">", ""));
+        answersIndex++;
+    }
+     int answer = int.Parse(Console.ReadLine());
+    if(answer == correctAnswerIndex)
+    {
+        Console.WriteLine("Correct!");
+        scores++;
+    }
+    else
+    {
+        Console.WriteLine("Incorrect");
+    }
+    Console.WriteLine("End of the Quiz! Your score was " + scores + " Points!");
+    Console.ReadKey();
+}
